bc65f0b471a1fce051531f7c1e664aa7919a0489
David Johnsonn <djohnso08@gmail.com>
2022-02-07 21:58:05 -0700

    add split method to parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,52 @@
+//UI Logic
+
+//Business Logic
 function creditCardValidator(numbers) {
-  let numbersArray = [];
-  
+  let str = numbers.split("");
+  let numbersArray = str;
+console.log(numbersArray);
 };
 
-creditCardValidator()
+creditCardValidator("4102080860435620")
+
+
+//Utility Logic
+
+//Potential Code to use for Business Logic
+
+// function creditCardValidator(numbers) {
+//   let numbersArray = [];
+//   creditCardValidator.forEach(function(element) {
+//     numbers.push(elements )
+//   })
+//   console.log(numbersArray)
+// };
+
+// creditCardValidator("4102080860435620")
+
+
+// function checkNumber() {
+//   let number = document.getElementById("creditCard").value;
+//   let multiplier = "2121212121212121";  // One more character added...
+//   let multipliedNumber;
+//   let sum = 0 // Initialise it as a number.
+
+//   if (number.length != 16) {
+//       //console.log("Please enter a Credit Card number that is 16 digits in ///length.");
+//   } else {
+//       for (count = 0; count < number.length; count++) { // Include last digit in loop
+//           multipliedNumber = number[count] * multiplier[count];
+//           if (multipliedNumber > 9) {
+//               // Use arithmetic to add the two digits
+//               multipliedNumber = multipliedNumber % 10 + Math.floor(multipliedNumber/10);
+//           }
+//           sum += multipliedNumber;
+//       }
+//       let check = sum % 10; // Simpler now because all digits were processed
+//       if (check == 0) { // Sum is multiple of 10
+//           console.log(`${number} is a valid Credit Card number.`);
+//       } else {
+//           console.log(`${number} is not a valid Credit Card number.`);
+//       }
+//   }
+// }
